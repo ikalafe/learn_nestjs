b1e26b0a135a0fe6c99b858666be64a5cf7f6a72
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-20 18:05:46 +0330

    Update PostController to use CreatePostDTO for request body in create method

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostDocument } from './schemas/post.schema';
+import { CreatePostDTO } from './dtos/create_post.dto';
 
 @Controller('posts')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Post()
-  create(@Body() requestBody: PostDocument) {
+  create(@Body() requestBody: CreatePostDTO) {
     requestBody.title
     return this.postService.create(requestBody);
   }
