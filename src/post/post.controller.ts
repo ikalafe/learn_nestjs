d5b6217d53ae57878e184b3a136132219c9a0b21
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-20 15:26:35 +0330

    Refactor PostController to add create and findAll methods with appropriate routes

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PostService } from './post.service';
+import { PostDocument } from './schemas/post.schema';
 
-@Controller()
+@Controller('posts')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
-  @Get('/posts')
-  getAll() {
-    return this.postService.getAll();
+  @Post()
+  create(@Body() requestBody: PostDocument) {
+    requestBody.title
+    return this.postService.create(requestBody);
+  }
+
+  @Get()
+  findAll() {
+    return this.postService.findAll();
   }
 }
