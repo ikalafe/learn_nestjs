8b3964ea558f0296605d715c1d71c83e0e1769d7
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-20 15:26:46 +0330

    Refactor PostService to integrate Mongoose for data management and add create and findAll methods

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,17 +1,19 @@
-import { ForbiddenException, HttpException, HttpStatus, Injectable, UnauthorizedException } from "@nestjs/common";
-
-const data = [
-  { id: 1, name: 'post1', description: 'post1 description' },
-  { id: 2, name: 'post2', description: 'post2 description' },
-  { id: 3, name: 'post3', description: 'post3 description' },
-  { id: 4, name: 'post4', description: 'post4 description' },
-  { id: 5, name: 'post5', description: 'post5 description' },
-];
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Post, PostDocument } from './schemas/post.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class PostService {
-  getAll() {
-    // throw new UnauthorizedException("daniyal");
-    return data;
+  constructor(@InjectModel(Post.name) private postModel: Model<Post>) {}
+
+  create(data: PostDocument) {
+    const createPost = new this.postModel(data);
+
+    return createPost.save();
+  }
+
+  async findAll(): Promise<Post[]> {
+    return this.postModel.find().exec();
   }
 }
