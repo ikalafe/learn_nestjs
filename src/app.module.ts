bbb8268418c31270f9283d549f479ebca18a0dd8
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-21 09:34:40 +0330

    Add HealthModule to the application imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { UserModule } from './user/user.module';
       'mongodb+srv://daniyaldehghan2021:5UcUZbsYRCGYBBNu@postclustor.jogj8.mongodb.net/post_db?retryWrites=true&w=majority&appName=PostClustor',
     ),
     UserModule,
+    HealthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
