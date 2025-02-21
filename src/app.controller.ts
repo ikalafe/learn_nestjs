fd3a3ad8da88617481f6e8f99f319b7c29d10eab
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-21 05:34:00 +0330

    Add TransformDTO interceptor to AppController for ResponsePostDTO transformation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TransformDTO } from './interceptors/transform_dto.interceptor';
+import { ResponsePostDTO } from './post/dtos/response_post.dto';
 
 @Controller()
+@TransformDTO(ResponsePostDTO)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
